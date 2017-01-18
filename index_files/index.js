ffd4bf8e65206742641313ac0b41df59a760cbf4
Randy <randy@randyedmonds.com>
2017-01-17 19:26:37 -0800

    first

---
+++ index_files/index.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20111209
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(1.0453,9.9452),color:'#000000',opacity:0.750000}),reflection_0:new IWReflection({opacity:0.50,offset:11.29}),stroke_0:new IWEmptyStroke(),reflection_1:new IWReflection({opacity:0.50,offset:11.34}),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(-0.3490,9.9939),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('index_files/indexMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');fixAllIEPNGs('Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
